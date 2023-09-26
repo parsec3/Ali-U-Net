507e10bb5748a166b38004fb8046509c0487f16b
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2023-09-26 23:00:42 +0200

    DNA_Generator.py

--- First Alignment Problem/DNA_Generator.py
+++ First Alignment Problem/DNA_Generator.py
@@ -9,11 +9,24 @@ Created on Tue Jul 25 14:41:18 2023
 
 import random
 import numpy as np
-
 import tensorflow as tf
-from tensorflow import keras
 
+#This code deals with the first alignment problem. We are dealing with sequences that have already been aligned by common alignment software (e.g. MAFFT), but with inaccurate gap insertion.
+#e.g.:
 
+#TTTGAC---GAGCAT
+#TTTG---ACAAGCAT
+#TTTGAC---TAGCAT
+
+#When it should be:
+
+#TTTGAC---GAGCAT
+#TTTGAC---AAGCAT
+#TTTGAC---TAGCAT
+
+#This part of the code will generate and store a matrix of DNAs representing both the correct and not-so-correct alignment.
+
+#This part generates four random numbers which we will use as weights to generate entirely random nucleotides (we need this later).
 
 def random_numbers_adding_up_100():
   while True:
@@ -33,16 +46,18 @@ def random_numbers_adding_up_100():
 
 gen = random_numbers_adding_up_100()
 
+#Here, we generate a DNA profile. We do not want to generate DNA entirely at random, as the sequences are supposed to have some degree of similarity (imagine them as being from closely related organisms).
+#So, in each column, 90% of all nucleotides will be the same. The remaining ones will represent point mutations that swap out one nucleotide for another.
+
 def DNA_profile(columns):
   probabilities = [0.90,0.04,0.03,0.03]
   profile=[]
   for i in range(columns):
-    prob = random.sample(probabilities, len(probabilities)) #Shuffle around so that the spotlight is always on a different nucleotide.
+    prob = random.sample(probabilities, len(probabilities)) #Shuffle around so that the 90% probability is always on a different nucleotide.
     profile+=[prob]
   return profile
 
 nucleotides = ["A", "C", "G", "T"]
-probabilities = [0.90,0.04,0.03,0.03]
 
 def make_sequences(rows,columns):
   profile = DNA_profile(columns)
@@ -51,14 +66,13 @@ def make_sequences(rows,columns):
     sequence = []
     for i in range(columns):
       sequence += random.choices(nucleotides, weights=profile[i], k=1)
-  #  sequence = ''.join(sequence)  ##I'll keep this as an on/off-switch in case we need the raw letters.
     sequences += [sequence]
   sequences = np.array(sequences)
   return sequences
 
-sequences = make_sequences(96,83)
+sequences = make_sequences(96,96) #You can do "print(sequences)" to see if it works as intended.
 
-def recode_seq(seq): #This one is for recoding everything.
+def recode_seq(seq): #This one is for recoding everything. We want to feed the data to a convolutional neural net later. Since it doesn't understand letters so well, it will receive numbers.
   seq_len = len(seq)
   a = np.empty(shape=(seq_len), dtype=np.uint8)
   for i, c in enumerate(seq):
@@ -74,29 +88,32 @@ def recode_seq(seq): #This one is for recoding everything.
       a[i] = 4
   return a
 
-recoded_seq = []
+recoded_seq = [] #Here, the numbers we generated above will be turned into unit vectors which, again, makes everything easier for the neural net later.
 for i in range(sequences.shape[0]):
   recoded_seq += [recode_seq(sequences[i])]
 
 recoded_seq = np.array(recoded_seq)
 
-shift_probabilities = [0.87,0.1,0.03]
+#We have a DNA matrix generator. Now, we want a way to add gaps to it.
+
+shift_probabilities = [0.87,0.1,0.03] #The gaps will be shifted from time to time to simulate the "wrong" alignment.
 shift = [0,2,1]
-probs = [0.90,0.04,0.03,0.03]
 
 def add_gaps(rows,columns):
   while True:
           sequence_array = make_sequences(rows,columns)
-          gap_number = 3
-          block_size = 10
+          gap_number = 3 #We have two blocks of gaps here, one consists of three gaps, the other of nine gaps.
+          block_size = 9
           gap = random.randint(10,int(columns/2))
           gap2 = random.randint(int(columns/2),(columns-10))
-          sequences = [] #This is all familiar
-          shift_sequences = [] #We create two sequences tis time!
+          sequences = [] #This list builds the correct alignment
+          shift_sequences = [] #And this one the incorrect one.
           for i in range(rows):
               sequence = list(sequence_array[i])
-              shift_sequence = list(sequence_array[i]) #Again, we need everything twice, this time.
-              if i < 3:
+              shift_sequence = list(sequence_array[i])
+              if i < 3: #The first three rows will be gap-free. Imagine this scenario: We are comparing the genomes of 96 organisms each. In the first three of them,
+                        #a random mutation has inserted several new nucleotides in the middle of the sequence. To make sure the DNA sequences are still homologous,
+                        #the alignment software must add gaps in order to preserve overlap.
                   gap_site = int(gap)
                   for j in range(block_size):
                     extra = ''.join(random.choices(nucleotides, weights=(next(random_numbers_adding_up_100())), k=1)) #Adds random nucleotides
@@ -108,12 +125,12 @@ def add_gaps(rows,columns):
                     sequence.insert(gap_site2,extra)
                     shift_sequence.insert(gap_site2,extra)
               else:
-                  random_shift = random.choices(shift, weights=shift_probabilities, k=1)[0] #A random number (0, 1, or 2) shifts the block to the left
+                  random_shift = random.choices(shift, weights=shift_probabilities, k=1)[0] #A random number (0, 1, or 2) shifts the gaps to the left
                   gap_site = int(gap) - random_shift  #Position of the gap (middle) - Shift number
                   for j in range(block_size):
                     sequence.insert(int(gap),'-') #Important! We need to leave it unchanged for the "normal" sequence
                     shift_sequence.insert(gap_site,'-')
-                  gap_site2 = int(gap2) - random_shift #For the block on the right
+                  gap_site2 = int(gap2) - random_shift #For the gaps on the right
                   for k in range(gap_number):
                     sequence.insert(int(gap2),'-')
                     shift_sequence.insert(gap_site2,'-')
@@ -127,7 +144,7 @@ def add_gaps(rows,columns):
           shift_sequences = np.array(shift_sequences)
           yield sequences, shift_sequences
 
-gap_sequence = add_gaps(96,83)
+gap_sequence = add_gaps(96,84)
 
 def add_unit_gaps(sequence_array): ##Now for the unit vector.
         sequences = [] #Build the array new.
@@ -139,6 +156,7 @@ def add_unit_gaps(sequence_array): ##Now for the unit vector.
         sequences = np.array(sequences)
         return sequences
 
+#The neural net needs a lot of data. Here, we create an array of 25,000 sequence-matrices to train the neural net, although much more can (and probably should) be generated for good results
 
 train_no_shift = np.empty((25000,96,96,5),dtype='uint8') #For the unshifted gaps.
 train_shift = np.empty((25000,96,96,5),dtype='uint8') #For the shifted gaps.
@@ -150,11 +168,6 @@ for i in range(25000):
   train_no_shift[i] = unit_gap_seq
   train_shift[i] = unit_shift_gap_seq
 
-#with open('shift_sequences.npy', 'wb') as f:
- #   np.save(f, train_no_shift)
-  #  np.save(f, train_shift)
-
-np.savez_compressed('compressed_shift_sequences.npz',x=train_shift, y=train_no_shift)
-
-#np.save('unshifted_small.npy', train_no_shift, allow_pickle=True, fix_imports=True)
-#np.save('shifted_small.npy', train_shift, allow_pickle=True, fix_imports=True)
+np.savez_compressed('compressed_shift_sequences.npz',x=train_shift, y=train_no_shift) #Here, we can save the matrices to our computer and even define an x and a y-axis.
+                                                                                      #The shifted sequences are the x-axis as those are the ones that the neural net receives.
+                                                                                      #The unshifted (correct) ones are the ones it has to predict.
