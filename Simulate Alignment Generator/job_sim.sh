f6fc7715a8b4dcc8529f01003d96eae9b7839e1d
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-01-28 22:54:25 +0100

    Update job_sim.sh

--- Simulate Alignment Generator/job_sim.sh
+++ Simulate Alignment Generator/job_sim.sh
@@ -1,13 +1,4 @@
 #!/bin/bash     #Shebang
-#
-#$ -cwd         #Execute job from current directory
-#$ -S /bin/bash #Interpeting shell for the job
-#$ -j n         #Do not join stdout and stderr
-#$ -N jobname #Name of the job
-#$ -m n        #Send mail at beginning and end of job.
-#$ -q fast.q
-#$ -pe smp 5
-
 
 conda activate tf
 
