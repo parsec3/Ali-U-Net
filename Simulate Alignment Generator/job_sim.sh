5dfbcfbb0222c7600070424433f9be809fc3ec51
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-01-28 22:43:16 +0100

    Update job_sim.sh

--- Simulate Alignment Generator/job_sim.sh
+++ Simulate Alignment Generator/job_sim.sh
@@ -12,11 +12,20 @@
 conda activate tf
 
 
-#python3 AliSim_RSE.py 48 48 5 3 alignments filename.tfr2
-#python3 AliSim_RSE.py 96 96 7 5 alignments filename.tfr2
+#python3 AliSim_RSE.py 48 48 5 3 10000000 48RSE_train.tfr2 #Training data
+#python3 AliSim_RSE.py 48 48 5 3 1000 48RSE_val.tfr2 #Validation data
 
-#python3 AliSim_RSE+IG.py 48 48 5 0 3 alignments filename.tfr2
-#python3 AliSim_RSE+IG.py 96 96 7 1 5 alignments filename.tfr2
+#python3 AliSim_RSE.py 96 96 7 5 10000000 96RSE_train.tfr2
+#python3 AliSim_RSE.py 96 96 7 5 1000 96RSE_val.tfr2
 
-#python3 AliSim_RSE_mixed.py 48 48 5 0 3 alignments filename.tfr2
-#python3 AliSim_RSE_mixed.py 96 96 7 1 5 alignments filename.tfr2
+#python3 AliSim_RSE+IG.py 48 48 5 0 3 10000000 48RSE-IG_train.tfr2
+#python3 AliSim_RSE+IG.py 48 48 5 0 3 1000 48RSE-IG_val.tfr2
+
+#python3 AliSim_RSE+IG.py 96 96 7 1 5 10000000 96RSE-IG_train.tfr2
+#python3 AliSim_RSE+IG.py 96 96 7 1 5 1000 96RSE-IG_val.tfr2
+
+#python3 AliSim_RSE_mixed.py 48 48 5 0 3 10000000 48RSE-mixed_train.tfr2
+#python3 AliSim_RSE_mixed.py 48 48 5 0 3 1000 48RSE-mixed_val.tfr2
+
+#python3 AliSim_RSE_mixed.py 96 96 7 1 5 10000000 96RSE-mixed_train.tfr2
+#python3 AliSim_RSE_mixed.py 96 96 7 1 5 1000 96RSE-mixed_val.tfr2
