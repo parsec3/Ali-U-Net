5322606c290a7342f6ca6444511bce65d67e248f
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-02-19 13:01:18 +0100

    Update tfr_to_fasta.sh

--- Simulate Alignment Generator/tfr_to_fasta.sh
+++ Simulate Alignment Generator/tfr_to_fasta.sh
@@ -1,14 +1,6 @@
-#!/bin/bash     #Shebang
-#
-#$ -cwd         #Execute job from current directory
-#$ -S /bin/bash #Interpeting shell for the job
-#$ -j n         #Do not join stdout and stderr
-#$ -N tfr_to_fasta #Name of the job
-#$ -m n        #Send mail at beginning and end of job.
-#$ -q fast.q
+#Job settings
 
 
 conda activate tf
 
-
-python3 tfr2fasta.py filename rows columns N
+python3 tfr2fasta.py filename rows columns 1000
