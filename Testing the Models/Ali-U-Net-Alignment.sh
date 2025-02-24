277409e83c3270392cbd06f44c1efa7fe383e094
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-02-24 23:29:48 +0100

    Update Ali-U-Net-Alignment.sh

--- Testing the Models/Ali-U-Net-Alignment.sh
+++ Testing the Models/Ali-U-Net-Alignment.sh
@@ -2,4 +2,4 @@
 
 conda activate tf
 
-#python3 Ali-U-Net-Alignment.py rows columns filepath/to/model
+#python3 Ali-U-Net-Alignment.py rows columns filepath/to/model filepath/to/input_file output_file
