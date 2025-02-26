25349b93c8f31fa2aa4ad1cf799bf8032902dd53
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-02-26 19:15:05 +0100

    Update Ali-U-Net-Score.py

--- Testing the Models/Ali-U-Net-Score.py
+++ Testing the Models/Ali-U-Net-Score.py
@@ -1,5 +1,7 @@
+import tensorflow as tf
 import numpy as np
 from statistics import median, mean
+import argparse
 
 nucleotide = ["A", "C", "G", "T", "-"]
 
@@ -20,20 +22,50 @@ def parse_fasta(file_path):
             sequences.append(sequence.upper())
     return sequences
 
-def make_predict_sequences(pred_array):
+def num_to_string(pred_array):
     sequences = []
     rows = pred_array.shape[0]
     columns = pred_array.shape[1]
     for i in range(rows):
-        weight_profile = pred_array[i]
         sequence = []
-        for j in range(columns):
-            probs = list(weight_profile[j])
-            position = probs.index(max(probs))
-            sequence += nucleotide[position]
-        sequence = ''.join(sequence)  # Create sequence string
-        sequences.append(sequence)
-    return np.array(sequences)
+        for j in pred_array[i]:
+            sequence += nucleotide[j]
+        sequence = ''.join(sequence)  ##I'll keep this as an on/off-switch in case we need the raw letters.
+        sequences += [sequence]
+    sequences = np.array(sequences)
+    return sequences
+
+def parse_tfrecord_fn(example):
+    print("parse_tfrecord_fn has been called.")
+    feature_description = {
+        'x': tf.io.FixedLenFeature([], tf.string),
+        'y': tf.io.FixedLenFeature([], tf.string),
+    }
+    example = tf.io.parse_single_example(example, feature_description)
+    x = tf.io.decode_raw(example['x'], tf.int8)
+    y = tf.io.decode_raw(example['y'], tf.int8)
+    x = tf.reshape(x, (48, 48))  # Assuming the shape of your data
+    y = tf.reshape(y, (48, 48))   # Assuming the shape of your data
+    return x, y
+
+def get_ref_alignment(data,N):
+    count = 0
+    aligned_sequences = []
+
+    for x_data, y_data in data:
+        aligned_sequences   += [num_to_string(y_data.numpy())]
+        count += 1
+        if count == N:
+            break
+
+    Y_seq = []
+
+    for y in aligned_sequences:
+        list_y = []
+        for b in y:
+            list_y += [b]
+        Y_seq += [list_y]
+    return Y_seq
 
 def calculate_pairwise_identity(seq1, seq2):
     if len(seq1) != len(seq2):
@@ -76,26 +108,39 @@ def calculate_column_score(sequences1, sequences2):
     column_score = (identical_columns / num_columns) * 100
     return column_score
 
-# Load the data
-loaded = np.load('filename.npz')
-y = loaded['y']
+parser = argparse.ArgumentParser(description="Load the model.")
+
+parser.add_argument("file", help="Path to tfrecord file file.")
+parser.add_argument("aligned_file", type=str,  help="The aligned fasta file produced by the Ali-U-Net.")
+
+args = parser.parse_args()
+
+filename = args.file
+filename = args.aligned_file
+
+# Create a TFRecordDataset
+dataset = tf.data.TFRecordDataset(filename, buffer_size=1000000)
+
+# Map the parse function to the dataset
+parsed_dataset = dataset.map(parse_tfrecord_fn).prefetch(100)
+parsed_ref_aligned_seq = get_ref_alignment(parsed_dataset,1000)
 
 Accuracy_List = []
 Accuracy_List2 = []
 Accuracy_List3 = []
 
 for i in range(1, 1001):
-    first_unit_shift_sequence = y[i-1]
-    first_shift_sequence = make_predict_sequences(first_unit_shift_sequence)
-    aligned_seq = parse_fasta(f"filepath/to/aligned_sequence_name_{i}.fasta")
+    ref_aligned_seq  = parsed_ref_aligned_seq[i-1]
+
+    AliU_aligned_seq = parse_fasta(aligned_file+f"_{i}.fasta")
 
     accurate_hits = 0
     inaccurate_hits = 0
 
-    for n, u in enumerate(aligned_seq):
+    for n, u in enumerate(AliU_aligned_seq):
         pred_seq = ''
-        nucl1 = list(u)[0:96]
-        nucl2 = list(first_shift_sequence[n])
+        nucl1 = list(u)
+        nucl2 = list(ref_aligned_seq[n])
         for m, v in enumerate(nucl1):
             if v == nucl2[m]:
                 pred_seq += v
@@ -110,19 +155,18 @@ for i in range(1, 1001):
 
     # Calculate sum-of-pairs score
     identity = []
-    for n, u in enumerate(aligned_seq):
-        remain = 96 - n
+    for n, u in enumerate(AliU_aligned_seq):
+        remain = len(AliU_aligned_seq) - n
         for i in range(remain-1):
-            identity_score = calculate_pairwise_identity(u, aligned_seq[n + i + 1])
-            if identity_score is not None:
-                identity.append(identity_score)
-    if identity:
-        Accuracy_List2.append(mean(identity))
-
+            identity_score = calculate_pairwise_identity(u,AliU_aligned_seq[n+i+1])
+            if identity_score != None:
+               identity += [identity_score]
+    Accuracy_List2.append(mean(identity))
+    
     # Calculate column score
-    column_score = calculate_column_score(aligned_seq, first_shift_sequence)
-    if column_score is not None:
-        Accuracy_List3.append(column_score)
+    column_score = calculate_column_score(AliU_aligned_seq,ref_aligned_seq)
+    if column_score != None:
+      Accuracy_List3.append(column_score)
 
 # Print results
 print(Accuracy_List)
