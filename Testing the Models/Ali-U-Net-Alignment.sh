10cf9549f2496d6b888e6d779be0f469957b92e6
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-02-27 11:08:53 +0100

    Update Ali-U-Net-Alignment.sh

--- Testing the Models/Ali-U-Net-Alignment.sh
+++ Testing the Models/Ali-U-Net-Alignment.sh
@@ -1,10 +1,10 @@
 ##Job Settings
 
-conda activate tf
-
 #Important: The input and output files have a name format of filename + number + ".fas", e.g. 48_RSE_test_x_3.fas
 #When giving the arguments to the model, it is important to only give the base name, as the script will add the number and the
 #file name extension (e.g. 48_RSE_test_x rather than 48_RSE_test_x_3.fas).
 #The same rule applies to "Ali-U-Net-Hallucination-Score.sh and Ali-U-Net-Score.sh"
+
+conda activate tf
 
 #python3 Ali-U-Net-Alignment.py rows columns filepath/to/model filepath/to/input_file output_file
