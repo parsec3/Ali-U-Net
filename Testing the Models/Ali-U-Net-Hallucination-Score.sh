7a76738e32bca499bf50ed3649e665e162af3c7a
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-02-27 11:08:39 +0100

    Update Ali-U-Net-Hallucination-Score.sh

--- Testing the Models/Ali-U-Net-Hallucination-Score.sh
+++ Testing the Models/Ali-U-Net-Hallucination-Score.sh
@@ -1,5 +1,7 @@
 ##Job settings
 
+#See "Ali-U-Net-Alignment.sh" for how to name the aligned_file."
+
 conda activate tf
 
 python3 Ali-U-Net-Hallucination-Score.py rows columns filepath/to/tfrecord_file filepath/to/aligned_file
