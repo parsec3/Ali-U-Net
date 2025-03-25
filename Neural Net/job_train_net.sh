f716aa4b167c167ff78f4e0449305232aebd281e
Petar Arsic <143598145+parsec3@users.noreply.github.com>
2025-03-25 19:34:03 +0100

    Update job_train_net.sh

--- Neural Net/job_train_net.sh
+++ Neural Net/job_train_net.sh
@@ -45,18 +45,17 @@ python Ali-U-Net-B2.py 96 96 sigmoid glorot_normal 96RSE-mixed_train.tfr2 96RSE-
 
 #python Ali-U-Net-B3.py rows colums activation_function initialization_function training_file.tfr2 test_file.tfr2 Checkpoint-file-path file-name
 
-python Ali-U-Net-B2.py 48 48 relu he_normal 48RSE_train.tfr2 48RSE_val.tfr2 48_RSE-relu-B3 Ali-U-Net_48x48_RSE-relu_B3_10M.h5
-python Ali-U-Net-B2.py 48 48 sigmoid glorot_normal 48RSE_train.tfr2 48RSE_val.tfr2 48_RSE-sigmoid-B3 Ali-U-Net_48x48_RSE-sigmoid_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 relu he_normal 96RSE_train.tfr2 96RSE_val.tfr2 96_RSE-relu-B3 Ali-U-Net_96x96_RSE-relu_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 sigmoid glorot_normal 96RSE_train.tfr2 96RSE_val.tfr2 96_RSE-sigmoid-B3 Ali-U-Net_96x96_RSE-sigmoid_B3_10M.h5
+python Ali-U-Net-B3.py 48 48 relu he_normal 48RSE_train.tfr2 48RSE_val.tfr2 48_RSE-relu-B3 Ali-U-Net_48x48_RSE-relu_B3_10M.h5
+python Ali-U-Net-B3.py 48 48 sigmoid glorot_normal 48RSE_train.tfr2 48RSE_val.tfr2 48_RSE-sigmoid-B3 Ali-U-Net_48x48_RSE-sigmoid_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 relu he_normal 96RSE_train.tfr2 96RSE_val.tfr2 96_RSE-relu-B3 Ali-U-Net_96x96_RSE-relu_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 sigmoid glorot_normal 96RSE_train.tfr2 96RSE_val.tfr2 96_RSE-sigmoid-B3 Ali-U-Net_96x96_RSE-sigmoid_B3_10M.h5
 
-python Ali-U-Net-B2.py 48 48 relu he_normal 48RSE+IG_train.tfr2 48RSE+IG_val.tfr2 48_RSE+IG-relu-B3 Ali-U-Net_48x48_RSE-IG-relu_B3_10M.h5
-python Ali-U-Net-B2.py 48 48 sigmoid glorot_normal 48RSE+IG_train.tfr2 48RSE+IG_val.tfr2 48_RSE+IG-sigmoid-B3 Ali-U-Net_48x48_RSE-IG-sigmoid_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 relu he_normal 96RSE+IG_train.tfr2 96RSE+IG_val.tfr2 96_RSE+IG-relu-B3 Ali-U-Net_96x96_RSE-IG-relu_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 sigmoid glorot_normal 96RSE+IG_train.tfr2 96RSE+IG_val.tfr2 96_RSE+IG-sigmoid-B3 Ali-U-Net_96x96_RSE-IG-sigmoid_B3_10M.h5
+python Ali-U-Net-B3.py 48 48 relu he_normal 48RSE+IG_train.tfr2 48RSE+IG_val.tfr2 48_RSE+IG-relu-B3 Ali-U-Net_48x48_RSE-IG-relu_B3_10M.h5
+python Ali-U-Net-B3.py 48 48 sigmoid glorot_normal 48RSE+IG_train.tfr2 48RSE+IG_val.tfr2 48_RSE+IG-sigmoid-B3 Ali-U-Net_48x48_RSE-IG-sigmoid_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 relu he_normal 96RSE+IG_train.tfr2 96RSE+IG_val.tfr2 96_RSE+IG-relu-B3 Ali-U-Net_96x96_RSE-IG-relu_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 sigmoid glorot_normal 96RSE+IG_train.tfr2 96RSE+IG_val.tfr2 96_RSE+IG-sigmoid-B3 Ali-U-Net_96x96_RSE-IG-sigmoid_B3_10M.h5
 
-python Ali-U-Net-B2.py 48 48 relu he_normal 48RSE-mixed_train.tfr2 48RSE-mixed_val.tfr2 48_RSE_mixed-relu-B3 Ali-U-Net_48x48_RSE_IG_mix-relu_B3_10M.h5
-python Ali-U-Net-B2.py 48 48 sigmoid glorot_normal 48RSE-mixed_train.tfr2 48RSE-mixed_val.tfr2 48_RSE_mixed-sigmoid-B3 Ali-U-Net_48x48_RSE_IG_mix-sigmoid_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 relu he_normal 96RSE-mixed_train.tfr2 96RSE-mixed_val.tfr2 96_RSE_mixed-relu-B3 Ali-U-Net_96x96_RSE_IG_mix-relu_B3_10M.h5
-python Ali-U-Net-B2.py 96 96 sigmoid glorot_normal 96RSE-mixed_train.tfr2 96RSE-mixed_val.tfr2 96_RSE_mixed-sigmoid-B3 Ali-U-Net_96x96_RSE_IG_mix-sigmoid_B3_10M.h5
-
+python Ali-U-Net-B3.py 48 48 relu he_normal 48RSE-mixed_train.tfr2 48RSE-mixed_val.tfr2 48_RSE_mixed-relu-B3 Ali-U-Net_48x48_RSE_IG_mix-relu_B3_10M.h5
+python Ali-U-Net-B3.py 48 48 sigmoid glorot_normal 48RSE-mixed_train.tfr2 48RSE-mixed_val.tfr2 48_RSE_mixed-sigmoid-B3 Ali-U-Net_48x48_RSE_IG_mix-sigmoid_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 relu he_normal 96RSE-mixed_train.tfr2 96RSE-mixed_val.tfr2 96_RSE_mixed-relu-B3 Ali-U-Net_96x96_RSE_IG_mix-relu_B3_10M.h5
+python Ali-U-Net-B3.py 96 96 sigmoid glorot_normal 96RSE-mixed_train.tfr2 96RSE-mixed_val.tfr2 96_RSE_mixed-sigmoid-B3 Ali-U-Net_96x96_RSE_IG_mix-sigmoid_B3_10M.h5
